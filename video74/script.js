36617d2fc3f2cb587abec304584d03ad376efccc
Shreyansh160 <sonishreyansh71@gmail.com>
2026-08-22 15:42:20 +0530

    Video74 Completed

---
+++ video74/script.js
@@ -0,0 +1,12 @@
+let button = document.getElementById("btn")
+// List of all mouse events 
+// https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
+button.addEventListener("click", ()=>{
+    document.querySelector(".box").innerHTML="<b>You were clicked</b> Enjoy your click!"
+})
+button.addEventListener("contextmenu", ()=>{
+    alert("Dont hack us by Right click Please")
+})
+document.addEventListener("keydown", (e)=>{
+    console.log(e, e.key, e.keyCode)
+})
