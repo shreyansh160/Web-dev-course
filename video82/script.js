89f98982fc13830c7e069d366e7588769b8e8bff
Shreyansh160 <sonishreyansh71@gmail.com>
2026-08-25 10:38:37 +0530

    Video82 Completed

---
+++ video82/script.js
@@ -0,0 +1,34 @@
+console.log(a1);
+async function sleep(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(45)
+        },1000);
+    })
+}
+
+function sum(a,b,c){
+    return a+b+c
+}
+
+(async function main(){
+    // let a = await sleep()
+    // console.log(a);
+    // let b = await sleep()
+    // console.log(b);
+
+    // let [x,y,...rest] =[1,5,8,9,4,6]
+    // console.log(x,y,rest=)
+
+    let obj ={
+        a:1,
+        b:2,
+        c:3
+    }
+    let {a,b}=obj
+    console.log(a,b)
+    let arr = [1,4,6]
+    // console.log(arr[0]+arr[1]+arr[2])
+    console.log(sum(...arr))
+})()
+var a1=6;
