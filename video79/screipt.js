852d326dfcdda464cc9d2a2690eb40343d3892ad
Shreyansh160 <sonishreyansh71@gmail.com>
2026-08-25 09:13:53 +0530

    Video79 Completed

---
+++ video79/screipt.js
@@ -0,0 +1,21 @@
+let a = prompt("Enter first number")
+let b = prompt("Enter second number")
+if(isNaN(a) || isNaN(b)){
+    throw SyntaxError("Sorrry this is not allowed")
+}
+let sum =parseInt(a)+parseInt(b)
+
+function main(){
+    let x=1;
+    try {
+        console.log("The sum is ", sum*x)
+        return true
+    } catch (error) {
+        console.error("Error")
+        return false
+    }
+    finally{
+        console.log("files are being closed and db connection is being closed")
+    }
+}
+let c =main()
